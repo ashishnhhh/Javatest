25520fd86d5fe4808afc9de0c00678ddcd1d070e
ashishnhhh <ashishnhansbhavi@gmail.com>
2024-05-14 16:51:24 +0530

    Update main.java
    Main

--- main.java
+++ main.java
@@ -1,5 +1,6 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("This Java project");
+        System.out.println("now u can see the changes in console output");
     }
 }
